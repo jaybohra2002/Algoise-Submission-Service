b4a98ff2f66674c410de0937a601a2f54234c58e
jaybohra2002 <jaybohra380@gmail.com>
2024-09-24 18:30:32 +0530

    Added connection to db

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const fastify= require('fastify')({logger:true });
-const serverConfig=require('./config/serverConfig')
+const serverConfig=require('./config/serverConfig');
+const connectDb=require('./config/dbConfig');
 const app=require('./app');
 fastify.register(app);
 
-fastify.listen({port:serverConfig.PORT},(err)=>{
+fastify.listen({port:serverConfig.PORT},async (err)=>{
     if(err){
         fastify.log.error(err);
         process.exit(1);
     }
+    await connectDb();
     console.log(`Server running on port ${serverConfig.PORT}`);
 });
 
